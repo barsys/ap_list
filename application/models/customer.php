c3a7091502808d4feb2609698b938281299cb0ed
Tetsuya Takahashi <takahashi_tetsuya@cygames.co.jp>
2014-11-20 15:26:40 +0900

    編集機能追加

--- application/models/customer.php
+++ application/models/customer.php
@@ -3,23 +3,37 @@
 
 class Customer extends CI_Model {
 
-    public function get_customer(){
-         $test = $this->db->get('customer');
-         return $test->result();
+    public function gets_customer(){
+         $customers = $this->db->get('customer');
+         return $customers->result();
 
     }
 
+    public function get_customer($cust_id){
+        $customer = $this->db->get_where('customer', array('cust_id' => $cust_id));
+        return $customer->result();
+    }
 
-    public function entry_customer($data){
+    public function entry($data){
         $this->db->set('company_name', $data['company_name']);
         $this->db->set('address', $data['address']);
         $this->db->set('tel', $data['tel']);
         $this->db->set('email', $data['email']);
         $this->db->set('person_in_charge', $data['person_in_charge']);
         $this->db->set('cust_status', $data['cust_status']);
-
+        $this->db->set('create_time', date("Y/m/d H:i:s"));
         $this->db->insert('customer');
+    }
 
+    public function edit($data){
+        $this->db->set('company_name', $data['company_name']);
+        $this->db->set('address', $data['address']);
+        $this->db->set('tel', $data['tel']);
+        $this->db->set('email', $data['email']);
+        $this->db->set('person_in_charge', $data['person_in_charge']);
+        $this->db->set('cust_status', $data['cust_status']);
+        $this->db->where('cust_id', $data['cust_id']);
+        $this->db->update('customer', $data);
     }
 
 }
