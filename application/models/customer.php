b7d8da8304dc8b248f9439422b08c75b937ad3b0
Tetsuya Takahashi <takahashi_tetsuya@cygames.co.jp>
2014-11-21 13:51:42 +0900

    削除実装、一覧表示対象修正

--- application/models/customer.php
+++ application/models/customer.php
@@ -4,7 +4,7 @@
 class Customer extends CI_Model {
 
     public function gets_customer(){
-         $customers = $this->db->get('customer');
+         $customers = $this->db->get_where('customer', array('delete_time' => NULL));
          return $customers->result();
 
     }
@@ -33,7 +33,12 @@ class Customer extends CI_Model {
         $this->db->set('person_in_charge', $data['person_in_charge']);
         $this->db->set('cust_status', $data['cust_status']);
         $this->db->where('cust_id', $data['cust_id']);
-        $this->db->update('customer', $data);
+        $this->db->update('customer');
     }
 
+    public function delete($cust_id){
+        $this->db->set('delete_time', date("Y/m/d H:i:s"));
+        $this->db->where('cust_id', $cust_id);
+        $this->db->update('customer');
+    }
 }
