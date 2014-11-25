6b5b053b35e08d556328db502d2c6e5cbffd1973
Tetsuya Takahashi <takahashi_tetsuya@cygames.co.jp>
2014-11-25 15:22:49 +0900

    ソース整理

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -2,47 +2,44 @@
 
 class Login extends CI_Controller {
 
-  public function __construct() {
-    parent::__construct();
-    $this->load->helper('url');
-    $this->load->library('session');
-  }
-
-  function entry() {
-    $this->load->view('login.html');
-  }
-
-  function logout() {
-    $this->session->sess_destroy();
-    redirect('login/index', 'location');
-  }
-
-  function is_login() {
-    $USER_STATUS = $this->session->userdata('USER_STATUS');
-    if ($USER_STATUS != 'LOGIN') {
-      redirect('login/entry', 'location');
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->library('session');
     }
-  }
 
-  public function exec_login() {
-    log_message('debug', "exec_login");
-    $account =  $this->input->post('account', true);
-    $password = $this->input->post('password', true);
-
-    if ( ($account === 'boo')&&($password === 'foo') ) {
-      log_message('debug', "TRUE");
-      $this->session->set_userdata('USERNAME', $account);
-      $this->session->set_userdata('USER_STATUS', 'LOGIN');
-      redirect('login/index', 'location');
-    } else {
-      log_message('debug', "ELSE");
-      $this->load->view('login.html');
+    function entry() {
+        $this->load->view('login.html');
     }
-  }
 
-  public function index() {
-    $this->is_login();
-    redirect('customer_info/index', 'location');
-    //$this->load->view('customer_list.html');
-  }
+    function logout() {
+        $this->session->sess_destroy();
+        redirect('login/index', 'location');
+    }
+
+    function is_login() {
+        $USER_STATUS = $this->session->userdata('USER_STATUS');
+        if ($USER_STATUS != 'LOGIN') {
+            redirect('login/entry', 'location');
+        }
+    }
+
+    public function exec_login() {
+        log_message('debug', "exec_login");
+        $user_name =  $this->input->post('account', true);
+        $password  = $this->input->post('password', true);
+
+        if ( ($user_name === 'boo')&&($password === 'foo') ) {
+            $this->session->set_userdata('USERNAME', $user_name);
+            $this->session->set_userdata('USER_STATUS', 'LOGIN');
+            redirect('login/index', 'location');
+        } else {
+          $this->load->view('login.html');
+        }
+    }
+
+    public function index() {
+        $this->is_login();
+        redirect('customer_info/index', 'location');
+    }
 }
---
+++ application/database/DML/user.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(32) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `company_name` varchar(64) NOT NULL,
+  `user_status` tinyint(4) NOT NULL,
+  `create_time` datetime NOT NULL,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `delete_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
